7cc10b3d2a06dd0485882cdb9522e2233366c469
smithhe <smithhe@purdue.edu>
2017-03-19 20:43:05 -0400

    Constructor Completed

--- Minheap.java
+++ Minheap.java
@@ -1,12 +1,18 @@
 
 public class Minheap {
+	private int heapSize;
+	private int numElements;
+	private Node heapList[];
 
 	/*public static void main(String[] args) {
 		
 	}*/
 	
 	public void newHeap() {
-		
+		this.heapSize = 20;
+		this.numElements = 0;
+		this.heapList = new Node[20];
 	}
-
+	
+	
 }
